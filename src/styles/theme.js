033219993fe71da88867957e7ad9c7fd2ac70fd9
igor <igorxavier96@gmail.com>
2023-01-07 11:51:11 -0300

    adicionar tema

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    BACKGROUND_100: "#3E3B47",
+    BACKGROUND_200: "rgba(255, 133, 155, 0.05)",
+  
+    WHITE_100: "#FFFFFF",
+    WHITE_200: "#F4EDE8",
+
+    PINK: "#FF859B",
+
+    GRAY_100: "#999591",
+    GRAY_200: "#948F99"
+  }
+
+  
+}

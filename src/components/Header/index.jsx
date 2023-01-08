226a17de7cd61fb0f10820479aefe4e654895a2a
igor <igorxavier96@gmail.com>
2023-01-08 10:46:01 -0300

    imagem do usuário

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from './styles';
+import { Container, Profile } from './styles';
 
 export function Header() {
   return(
-    <Container>
-      
+    <Container>   
+      <Profile>
+        <img 
+          src="https://github.com/igor1203.png" 
+          alt="Imagem do GitHub"
+        />
+
+        <div>
+          <span>Igor Xavier</span>
+          <strong>sair</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -14,4 +14,34 @@ export const Container = styled.header`
   justify-content: space-between;
 
   padding: 0 83px;
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 18px;
+  }
+
+  span {
+    font-size: 14px;
+    color: ${({  theme }) => theme.COLORS.WHITE_200};
+    font-weight: 700;
+  }
+
+  strong {
+    font-size: 14px;
+    color: ${({  theme }) => theme.COLORS.GRAY_200};
+    font-weight: 400;
+  }
 `

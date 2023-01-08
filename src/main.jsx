48a08cbcfd3c085878d3a3dad6fc9b55b664154d
igor <igorxavier96@gmail.com>
2023-01-08 09:47:59 -0300

    componente header

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
 import { Details } from './pages/Details'
+import { Header } from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Header />
       <Details />
     </ThemeProvider>
   </React.StrictMode>,
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,11 +5,7 @@ import { Button } from '../../components/Button'
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
-      <Button title="Salvar" />
+      
     </Container>
   )
 }

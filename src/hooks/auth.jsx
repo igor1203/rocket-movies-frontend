0f1308b4ec7d8a715ef3c828e278b1770095de07
igor <igorxavier96@gmail.com>
2023-04-21 12:22:29 -0300

    Atualizar perfil do Usuário

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -35,6 +35,23 @@ function AuthProvider({ children }) {
     setData({})
   }
 
+  async function updateProfile({ user }){
+  try {
+    await api.put("/users", user)
+    localStorage.setItem("@rocketmovies:user", JSON.stringify(user))
+
+    setData({ user, token: data.token })
+    alert("Perfil atualizado!")
+
+  } catch (error) {
+    if(error.response){
+      alert(error.response.data.message)
+    }else{
+      alert("Não foi possível atualizar o perfil")
+    }
+  }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketmovies:token")
     const user = localStorage.getItem("@rocketmovies:user")
@@ -53,6 +70,7 @@ function AuthProvider({ children }) {
     <AuthContext.Provider value={{
       signIn,
       signOut,
+      updateProfile,
       user: data.user,
     }}  
     >
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -2,18 +2,28 @@ import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 import { ButtonText } from '../../components/ButtonText'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
-import { useAuth } from '../../hooks/auth'
 import { Container, Avatar } from './styles'
-import { useState } from 'react'
+import { useAuth } from '../../hooks/auth'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export function Profile() {
-const { user } = useAuth()
+const { user, updateProfile } = useAuth()
 
   const [name, setName] = useState(user.name)
   const [email, setEmail] = useState(user.email)
   const [passwordOld, setPasswordOld] = useState()
   const [passwordNew, setPasswordNew] = useState()
+
+  async function handleUpdate(){
+    const user ={
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld,
+    }
+    await updateProfile({ user })
+  }
 
   return(
     <Container>
@@ -76,7 +86,7 @@ const { user } = useAuth()
           onChange={e => setPasswordNew(e.target.value)}
         />
 
-        <Button title={"Salvar"} />
+        <Button title="Salvar" onClick={handleUpdate} />
 
       </form>
     </main>

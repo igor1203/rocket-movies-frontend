3abefb23db909011acd534e1c500f738b5cbedc8
igor <igorxavier96@gmail.com>
2023-04-22 10:50:14 -0300

    Adicionar tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,13 +1,23 @@
-import { Container } from './styles'
-import { Header } from '../../components/Header'
-import { FiArrowLeft } from 'react-icons/fi'
 import { ButtonText } from "../../components/ButtonText"
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
 import { MovieItem } from '../../components/MovieItem'
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+import { FiArrowLeft } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+import { Container } from './styles'
+import { useState } from 'react'
 
 export function New() {
+
+  const [tags, setTags] = useState([])
+  const [newTag, setNewTag] = useState("")
+
+  function handleAddTag(){
+    setTags(prevState => [...prevState, newTag])
+    setNewTag("")
+  }
+
   return (
     <Container>
       <Header />
@@ -44,10 +54,23 @@ export function New() {
           <section title='Marcadores'>
             <h2>Marcadores</h2>
             <div className='tags'>
-             <MovieItem 
-              value="React" />
+              {
+                tags.map((tag, index) => (
+                  <MovieItem
+                    key={String(index)} 
+                    value={tag}
+                    onClick={() => {}}
+                    />
+                ))
+              }
+
             <MovieItem 
-              isNew placeholder="Novo marcador"/>    
+              isNew
+              placeholder="Novo marcador"
+              onChange={e => setNewTag(e.target.value)}
+              value={newTag}
+              onClick={handleAddTag}
+              />    
             </div>
           </section>
 
